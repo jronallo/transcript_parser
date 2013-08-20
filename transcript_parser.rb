839fcf2a51cc6d45183917cc4050240ed3b97160
Jason Ronallo <jronallo@gmail.com>
2013-08-20 15:27:48 -0400

    initial commit

---
+++ transcript_parser.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+# transcript_parser.rb
+# A simple script to parse a plain text transcript of a particular format into webvtt objects
+# with some other data.
+# Does not serialize back into WebVTT though.
+
+require 'webvtt'
+require 'pp'
+require 'pry'
+
+filepath = ARGV[0]
+file = File.read(filepath)
+
+first_cue = Webvtt::Cue.new
+first_cue.start = '00:00:00'
+cues = [first_cue]
+
+interview = file.match(/START OF INTERVIEW(.*)END OF INTERVIEW/m)[1]
+
+interview.chomp!
+
+interview_lines = interview.each_line.to_enum
+interview_lines.with_index do |line, index|
+  if !line.chomp.empty?
+    timestamp_match = line.match(/\[\[(.*)\]\]/)
+    if timestamp_match
+      cues.last.end = timestamp_match[1]
+      if interview_lines.count != index + 1
+        cues << Webvtt::Cue.new
+        cues.last.start = timestamp_match[1]
+      end
+    else
+      cues.last.text ||= ''
+      cues.last.text << line
+    end
+  end
+end
+
+
+
+pp cues
+
